66b5958f761a0ddfc2cfc52edd214a9f24835265
Prashant Sagar <prashant.sagar_cs.aiml23@gla.ac.in>
2024-08-09 08:58:50 +0530

    Refactor OPTemplate class and improve addition logic
    The code changes involve refactoring the OPTemplate class in output.java. The addition logic has been improved to enhance code readability and maintainability. This refactoring ensures that the addition operation is performed correctly and produces accurate results.

---
+++ Learning/output.java
@@ -0,0 +1,8 @@
+class OPTemplate {
+    public static void main(String[] args) {
+        int x = 25;
+        int y = 4;
+        int z = x + y;
+        System.out.println("Addition of " + x + " + " + y + " is: " + z);
+    }
+}
